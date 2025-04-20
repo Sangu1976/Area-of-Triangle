6b214991d7b4e230dbf9bdc7382f97c088263a0b
Sangeetha Joseph <sangeethajoseph76@gmail.com>
2025-04-20 20:14:44 +0200

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Area of Traingle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Area of Traingle")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1936caaf-ee54-4166-b13a-06cc2b0e1596")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Area_of_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the base of the triangle: ");
+            double baseLength = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the height of the triangle: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double area = 0.5 * baseLength * height;
+
+            Console.WriteLine($"The area of the triangle is: {area}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
